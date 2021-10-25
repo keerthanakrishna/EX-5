4958564e4a65bbd6ddee3544d8ad99621519618d
keerthanakrishna <80613387+keerthanakrishna@users.noreply.github.com>
2021-10-25 07:29:24 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,35 @@
+var http = require('http'); 
+var fs = require('fs');
+const PORT= process.env.PORT||5500;
+var server = http.createServer(function (req, res) {  
+    if (req.url == '/') { 
+            res.writeHead(200,{'Content-type':'text/html'});
+            
+            fs.readFile('./index.html',null,function(error,data){
+
+                if(error){
+                    res.writeHead(404);
+                    res.write('File not Found!')
+                }else{
+                    res.write(data);   
+                }
+        res.end();
+            });
+    }
+    else{
+        res.writeHead(200,{'Content-type':'text/html'});
+        fs.readFile('./last.html',null,function(error,data){
+            if(error){
+                res.writeHead(404);
+                res.write('File not Found!')
+            }else{
+                res.write(data);
+    
+            }
+    res.end();
+        });
+    }
+});
+server.listen(PORT,()=>{ 
+console.log('Server running on port ' + PORT)
+});
